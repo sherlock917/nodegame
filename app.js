45d2d4afb0adee560f953d1d3b8b8a23765c35a8
Sherlock Zhong <sherlock.z0917@gamil.com>
2014-04-12 13:12:25 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+var express = require('express');
+var logfmt = require('logfmt');
+
+var routes = require('./routes');
+
+var app = express();
+
+var port = Number(process.env.PORT || 5000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logfmt.requestLogger());
+
+app.use('/', routes);
+
+app.listen(port, function () {
+    console.log('listening on port ' + port);
+});
+
+module.exports = app;
